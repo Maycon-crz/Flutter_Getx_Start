d1ab0ddc0b0bd9b1866086dab09354b715db1707
FlutLab.io <>
2022-02-28 19:01:35 +0000

    Autosave

---
+++ lib/app/view/consumindo_api_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class ConsumindoApi extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('GetConnection Tutprial'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Tutorial GetConnect',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
